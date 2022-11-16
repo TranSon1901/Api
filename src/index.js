7893e0bbe5caeba2a0b7c2be7d94c05cd6f0fb04
transon <transon1901@gmail.com>
2022-11-16 11:49:10 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import router from './routes/index'
-
+import mongoose from 'mongoose'
 const app=express()
 
 dotenv.config()
 const port=  process.env.PORT
+const DB_CONNET= process.env.DB_CONNET
+mongoose.connect(DB_CONNET,
+    {useNewUrlParser: true },
+    ()=>console.log('connnet to Db'))
 
 router(app)
 app.listen(port,()=>{
